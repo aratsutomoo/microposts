ef093f7a0b9856c0263e97976b09d24af1a5c52b
tomoo.arastu <aratsu435@gmail.com>
2020-11-23 00:39:04 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
-<?php
+@extends('layouts.app')
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+@section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Microposts</h1>
+        </div>
+    </div>
+@endsection
