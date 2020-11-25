86674797cabf010806de5fd07d317861fd2f175b
tomoo.arastu <aratsu435@gmail.com>
2020-11-25 10:53:59 +0000

    post

--- app/User.php
+++ app/User.php
@@ -8,7 +8,17 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    
     use Notifiable;
+     public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
 
     /**
      * The attributes that are mass assignable.
