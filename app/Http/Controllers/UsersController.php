313a7e57ce77e8bf338cdccb7a796800464902b8
tomoo.arastu <aratsu435@gmail.com>
2020-12-05 12:43:46 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,15 +8,23 @@ use App\User; // 追加
 
 class UsersController extends Controller
 {
-    public function index()
+     public function index()
     {
-        // ユーザ一覧をidの降順で取得
-        $users = User::orderBy('id', 'desc')->paginate(10);
+        $data = [];
+        if (\Auth::check()) {
+            // 認証済みユーザ（閲覧者）を取得
+            $user = \Auth::user();
+            // ユーザとフォロー中ユーザの投稿の一覧を作成日時の降順で取得
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        // ユーザ一覧ビューでそれを表示
-        return view('users.index', [
-            'users' => $users,
-        ]);
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+            ];
+        }
+
+        // Welcomeビューでそれらを表示
+        return view('welcome', $data);
     }
     
    public function show($id)
--- app/User.php
+++ app/User.php
@@ -21,6 +21,16 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
     }
+    
+     public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 
     /**
      * このユーザをフォロー中のユーザ。（ Userモデルとの関係を定義）
